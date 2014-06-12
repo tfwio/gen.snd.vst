9627367f804c9e1cdc2ee79e0061791ecbbe05a9
tfoxo <tfwroble@gmail.com>
2014-06-12 08:21:35 -0500

    static version no; fix naudio asm-reference

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.CompilerServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.1")]
 //[assembly: InternalsVisibleTo(@"Generator.Lib, PublicKey=00240000048000009400000006020000002400005253413100040000010001001fc2e9a138afaa2989d35c910f228841cd565ebc7bebecb39ee2af9ecfd8e1a6db1ac79c4d7c83ab6bb83993a9a7b8be0703a251b53d279f64305828dc29da0b63dd857dddbdd5081c491873a2b350a1eeda02fc55711daac277301b1e6c5a37a762dec6550252b8d2140926c1634a76515ac7ece4fe9419dbac3b65a0d48c86")]
